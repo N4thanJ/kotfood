242574098e69dfc3525a16d273e105fa59dcb392
N4thanJ <nathan.jordens@student.ucll.be>
2025-09-14 14:29:00 +0200

    Added divider to admin panel

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -6,12 +6,14 @@ export default function AdminSidebar() {
     <aside className='sticky top-0 flex h-screen w-64 flex-col bg-white shadow-md dark:bg-green-900'>
       <div className='flex items-center justify-between p-4'>
         <div className='flex flex-col'>
-          <h1 className='text-lg font-bold text-green-700 dark:text-green-200'>
-            Kotfood
-          </h1>
+          <h1 className='text-4xl font-black'>KotFood</h1>
           <span className='text-sm text-gray-500 dark:text-gray-400'>
             Admin
           </span>
+          {/* Divider */}
+          <div
+            className={`mt-2 h-1 w-16 rounded-sm bg-green-500 opacity-80 dark:bg-yellow-200`}
+          ></div>
         </div>
       </div>
 
